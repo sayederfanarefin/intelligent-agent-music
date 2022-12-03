81b0603e8e4eaa23fc0da7da36aa1a075469dc9c
hruday <ramahrudayb@gmail.com>
2022-12-02 20:39:24 -0600

    add posting code to loclhost:8001

--- src/components/code-form.js
+++ src/components/code-form.js
@@ -1,17 +1,36 @@
-import Form from 'react-bootstrap/Form';
-
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 function CodeForm() {
+  const [code, setCode] = useState("");
+
+  const submit = () => {
+    fetch("http://localhost:8001/", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        code,
+      }),
+    });
+  };
+
   return (
     <Form>
-      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="name@example.com" />
-      </Form.Group>
       <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
         <Form.Label>Example textarea</Form.Label>
-        <Form.Control as="textarea" rows={3} />
+        <Form.Control
+          as="textarea"
+          rows={3}
+          onChange={(e) => setCode(e.target.value)}
+        />
       </Form.Group>
+      <Button variant="primary" onClick={() => submit()}>
+        Submit
+      </Button>
     </Form>
   );
 }
